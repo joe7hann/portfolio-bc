c6c57c87fd78efb1b22733273ecf16657101c3a3
Johann <joe7hann@gmail.com>
2022-12-11 17:55:23 -0500

    collectstatic

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -139,6 +139,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'portfolio/static'),)
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -146,3 +147,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL ='login'
 LOGIN_REDIRECT_URL = "admin_index"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
